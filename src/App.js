1aa7ad21e203527d655f567453be7fd7062896d4
Kunal Khatri <kunalkhatri137@gmail.com>
2022-12-04 00:57:02 +0530

    Navbar and router added

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
-import './App.css';
-import { NotifCard } from './components/NotifCard/NotifCard';
-import {useState, useEffect} from 'react';
-const PushAPI = require('@pushprotocol/restapi');
+import "./App.css"
+import { NotifCard } from "./components/NotifCard/NotifCard"
+import { useState, useEffect } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage"
+import ConsumerPage from "./pages/ConsumerPage/ConsumerPage"
+import UserPage from "./pages/UserPage/UserPage"
+import { MoralisProvider } from "react-moralis"
+const PushAPI = require("@pushprotocol/restapi")
 
 function App() {
-  const [notifs, setNotifs] = useState([])
-  const Alerts = () => {
-    useEffect(() => {
-        const fetchNotifs = async (
-          chainId = "8001",
-          public_key = "0x5E4EE2aA55C20cae19eb8592aC8216264F9813dE",
-          environment = "staging"
-        ) => {
-          try {
-            const notifications = await PushAPI.user.getFeeds({
-              user: `eip155:${chainId}:${public_key}`, // user address in CAIP
-              env: `${environment}`,
-            });
-        
-            console.log(notifications);
-            setNotifs(notifications);
-          } catch (error) {
-            console.log(
-              "There was some issue getting the Notifications. Error: ",
-              error
-            );
-            return;
-          }
-        };
-        
-        fetchNotifs()
-        const interval = setInterval(() => fetchNotifs(), 2000)
-        return () => {
-          clearInterval(interval);
-        }
-    }, [])}
+    const [notifs, setNotifs] = useState([])
+    const Alerts = () => {
+        useEffect(() => {
+            const fetchNotifs = async (
+                chainId = "8001",
+                public_key = "0x5E4EE2aA55C20cae19eb8592aC8216264F9813dE",
+                environment = "staging"
+            ) => {
+                try {
+                    const notifications = await PushAPI.user.getFeeds({
+                        user: `eip155:${chainId}:${public_key}`, // user address in CAIP
+                        env: `${environment}`,
+                    })
 
-    Alerts();
+                    console.log(notifications)
+                    setNotifs(notifications)
+                } catch (error) {
+                    console.log("There was some issue getting the Notifications. Error: ", error)
+                    return
+                }
+            }
 
-  return (
-    <div className="App">
-      <h1>Pending Requests</h1>
-      <div className="notifStack">
-        {
-          // notifs.map(notif => {
-          //   return (<NotifCard notifTitle={notif.title} notifMessage={notif.message} />)
-          // })
-          <NotifCard notifTitle="this is the notificaiton title" notifMessage="this is notif message" />
-        }
-      </div>
-    </div>
-  );
+            fetchNotifs()
+            const interval = setInterval(() => fetchNotifs(), 2000)
+            return () => {
+                clearInterval(interval)
+            }
+        }, [])
+    }
+
+    Alerts()
+
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />}></Route>
+                    <Route path="/user" element={<UserPage />}></Route>
+                    <Route path="/consumer" element={<ConsumerPage />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </MoralisProvider>
+
+        // <div className="App">
+        //     <h1>Pending Requests</h1>
+        //     <div className="notifStack">
+        //         {
+        //             // notifs.map(notif => {
+        //             //   return (<NotifCard notifTitle={notif.title} notifMessage={notif.message} />)
+        //             // })
+        //             <NotifCard
+        //                 notifTitle="this is the notificaiton title"
+        //                 notifMessage="this is notif message"
+        //             />
+        //         }
+        //     </div>
+        // </div>
+    )
 }
 
-export default App;
+export default App
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,20 +1,16 @@
-import React from "react";
-import Navbar from "../../components/NavBar/Navbar";
-import { useMoralis } from "react-moralis";
+import React from "react"
+import Navbar from "../../components/NavBar/Navbar"
+// import { useMoralis } from "react-moralis";
 
 const HomePage = () => {
-  const { isWeb3Enabled } = useMoralis();
+    // const { isWeb3Enabled } = useMoralis();
 
-  return (
-    <>
-      <Navbar />
-      {isWeb3Enabled ? (
-        <div>Web3 is enabled</div>
-      ) : (
-        <h1>Please connect wallet</h1>
-      )}
-    </>
-  );
-};
+    return (
+        <>
+            <Navbar />
+            {/* {isWeb3Enabled ? <div>Web3 is enabled</div> : <h1>Please connect wallet</h1>} */}
+        </>
+    )
+}
 
-export default HomePage;
+export default HomePage

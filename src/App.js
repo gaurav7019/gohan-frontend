f165ec900221a4903853d9d80e2c595757b6650c
Kunal Khatri <kunalkhatri137@gmail.com>
2022-12-04 01:00:35 +0530

    refactor code app.js

--- src/App.js
+++ src/App.js
@@ -9,38 +9,6 @@ import { MoralisProvider } from "react-moralis"
 const PushAPI = require("@pushprotocol/restapi")
 
 function App() {
-    const [notifs, setNotifs] = useState([])
-    const Alerts = () => {
-        useEffect(() => {
-            const fetchNotifs = async (
-                chainId = "8001",
-                public_key = "0x5E4EE2aA55C20cae19eb8592aC8216264F9813dE",
-                environment = "staging"
-            ) => {
-                try {
-                    const notifications = await PushAPI.user.getFeeds({
-                        user: `eip155:${chainId}:${public_key}`, // user address in CAIP
-                        env: `${environment}`,
-                    })
-
-                    console.log(notifications)
-                    setNotifs(notifications)
-                } catch (error) {
-                    console.log("There was some issue getting the Notifications. Error: ", error)
-                    return
-                }
-            }
-
-            fetchNotifs()
-            const interval = setInterval(() => fetchNotifs(), 2000)
-            return () => {
-                clearInterval(interval)
-            }
-        }, [])
-    }
-
-    Alerts()
-
     return (
         <MoralisProvider initializeOnMount={false}>
             <BrowserRouter>
@@ -51,21 +19,6 @@ function App() {
                 </Routes>
             </BrowserRouter>
         </MoralisProvider>
-
-        // <div className="App">
-        //     <h1>Pending Requests</h1>
-        //     <div className="notifStack">
-        //         {
-        //             // notifs.map(notif => {
-        //             //   return (<NotifCard notifTitle={notif.title} notifMessage={notif.message} />)
-        //             // })
-        //             <NotifCard
-        //                 notifTitle="this is the notificaiton title"
-        //                 notifMessage="this is notif message"
-        //             />
-        //         }
-        //     </div>
-        // </div>
     )
 }
 
